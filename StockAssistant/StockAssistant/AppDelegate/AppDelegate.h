a206c65ee7d092b7f668f716ec6e882b0bdd9a4b
周鑫 <15901263220@163.com>
2018-12-10 20:16:03 +0800

    项目初始化

---
+++ StockAssistant/StockAssistant/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StockAssistant
+//
+//  Created by 周鑫 on 2018/12/10.
+//  Copyright © 2018 TJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
